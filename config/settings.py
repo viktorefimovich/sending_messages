6ed6da204f56b6dce21e2f38c91dce2054cf125d
Viktor Vershinin <viktorefimovich@bk.ru>
2025-07-23 17:17:26 +0900

    Добавлено приложение рассылки

--- config/settings.py
+++ config/settings.py
@@ -113,3 +113,7 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
