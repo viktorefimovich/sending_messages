2501177e0c8ae4880ac733a6e2a8b89bc6ce09ba
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-18 17:22:40 +0800

    добавлен services.py

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
