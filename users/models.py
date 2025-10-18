86ad799d4b7790cec980d4c340d830e6ba552edd
Viktor Vershinin <viktorefimovich@bk.ru>
2025-10-18 19:06:55 +0800

    исправлено

--- users/models.py
+++ users/models.py
@@ -8,6 +8,20 @@ class User(AbstractUser):
                                     help_text="Введите номер телефона")
     avatar = models.ImageField(upload_to="users/avatars/", verbose_name="Аватар", blank=True, null=True,
                                help_text="Загрузите свой аватар")
+    country = models.CharField(
+        max_length=20,
+        verbose_name="страна проживания",
+        blank=True,
+        null=True,
+        default="Неизвестно",
+    )
+
+    token = models.CharField(
+        max_length=100,
+        verbose_name="Токен для подтвержения почты",
+        blank=True,
+        null=True,
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "phone_number"]
@@ -15,6 +29,7 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+        permissions = [("can_manage_users", "Управление пользователем")]
 
     def __str__(self):
         return self.email
